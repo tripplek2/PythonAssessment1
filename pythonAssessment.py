a5895648b802d75f7ea4fc61a2e13c38a6377b3d
Kelvin Korir <kibetkelvin222@gmail.com>
2026-06-02 10:59:53 +0300

    added the find most common word and word length average functionality

--- pythonAssessment.py
+++ pythonAssessment.py
@@ -9,3 +9,43 @@ def read_article(filename):
 def count_specific_word(text, search_word):
     words = re.findall(r"\b\w+\b", text.lower())
     return words.count(search_word.lower())
+
+# Find the most common word
+def identify_most_common_word(text):
+    words = re.findall(r"\b\w+\b", text.lower())
+
+    if len(words) == 0:
+        return None
+    
+    word_count = {}
+
+    # for loop
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    
+    most_common = max(word_count, key=word.count.get)
+
+    return most_common
+
+# Calculate average word length
+def calculate_average_word_length(text):
+
+    if text.strip() == "":
+        return 0
+    
+    words = re.findall(r"\b\w+\b", text)
+
+    if len(words) == 0:
+        return 0
+    
+    total_letters = 0
+
+    for word in words:
+        total_letters += len(word)
+    
+    average = total_letters / len(words)
+
+    return average
